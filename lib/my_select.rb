13b645490cab88f4f28efde3f3354a219e671945
Richard Chen <richard.c.chen@gmail.com>
2018-07-24 01:47:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+    i = 0
+    newarray = []
+    while i < collection.length
+      if yield(collection[i]) 
+        newarray.push(collection[i])
+      end
+      i = i + 1
+    end
+     newarray
 end
